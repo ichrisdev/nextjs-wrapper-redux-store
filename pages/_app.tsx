b01736b234c0b8f80212d2ea6f07fa1aaceacb1d
ichrisdev <chrmesina@icloud.com>
2022-10-26 17:48:15 -0300

    fix: You are using legacy implementaion. Please update your code: use createWrapper() and wrapper.useWrappedStore()

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { wrapper } from '../store/store';
+import { Provider } from 'react-redux';
 
 function App({ Component, pageProps }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(pageProps)
   return (
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
   )
 }
 
